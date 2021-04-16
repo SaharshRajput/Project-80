f0778685a4131fe77b0f5ee8031721880d5e25fc
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-04-16 17:49:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-var inputs=[];
+
 
 
 function get_para_1()
 {
-    for(i=1 ;i<=6;i++)
+    var inputs=[];
+    for(var i=1 ;i<=6;i++)
     {
     inputs.push(document.getElementById("input_box_"+i).value);
     }
-    document.getElementById("show_para_1").innerHTML=inputs.join(",");
+    document.getElementById("show_para_1").innerHTML=inputs.join(". ");
 }
 function get_para_2()
 {
-    for(i=1 ;i<=6;i++)
+    var inputs=[];
+    for(var i=1 ;i<=6;i++)
     {
     inputs.push(document.getElementById("para_input_box_"+i).value);
     }
